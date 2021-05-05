930bc57dccaa02152d1bb5e5782f0d091f92801e
LaScheggia <lascheggia605@gmail.com>
2021-05-05 18:30:50 +0200

    v a p o r

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,36 +4,44 @@ const app = new Vue({
     counterCar: 0,
 
     photos: [
-      'assets\css\img\cadillacCoupedeville.png',
-      'assets\css\img\cadillacEldorado.png',
-      'assets\css\img\cadillacOldtimer.png',
-      'assets\css\img\chevrolet1957.png',
-      'assets\css\img\chevroletBelAir.png',
-      'assets\css\img\chevroletChevelle.png',
-      'assets\css\img\chevroletImpala.png',
-      'assets\css\img\fordThunderbolt.png',
-      'assets\css\img\plymouthRoadRunner.png',
-      'assets\css\img\shelbyCobra.png'
+      'assets/css/img/cadillacCoupedeville.png',
+      'assets/css/img/cadillacEldorado.png',
+      'assets/css/img/cadillacOldtimer.png',
+      'assets/css/img/chevrolet1957.png',
+      'assets/css/img/chevroletBelAir.png',
+      'assets/css/img/chevroletChevelle.png',
+      'assets/css/img/chevroletImpala.png',
+      'assets/css/img/fordThunderbolt.png',
+      'assets/css/img/plymouthRoadRunner.png',
+      'assets/css/img/shelbyCobra.png'
     ],
 
+
   },
+
+  mounted: 
+    function(){
+      setInterval(this.nextCar, 3000)
+    },
+
   
+
   methods: {
     nextCar(){
       this.counterCar++;
-      if(this.counterCar === this.photos.lenght) this.counterCar = 0;
-
-
+      if(this.counterCar === this.photos.length) this.counterCar = 0;
     },
+
     prevCar(){
       this.counterCar--;
-      if(this.counterCar < 0) this.counterCar = this.photos.lenght -1;
+      if(this.counterCar < 0) this.counterCar = this.photos.length -1;
     },
-  
+
+    autoplay(){
+      setInterval(this.nextCar, 3000)
+    },
+
   },
-
-
-  
 
 });
 
